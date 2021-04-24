2129f0c46508e6a4771b0aed60875926c8f74c11
alemi <me@alemi.dev>
2021-04-25 00:18:14 +0200

    added old iter_history based freq

--- generators.py
+++ generators.py
@@ -18,7 +18,7 @@ from bot import alemiBot
 
 from util.permission import is_allowed
 from util.message import edit_or_reply, is_me
-from util.getters import get_text
+from util.getters import get_text, get_username, get_channel
 from util.text import tokenize_json, cleartermcolor
 from util.command import filterCommand
 from util.decorators import report_error, set_offline
@@ -315,3 +315,52 @@ async def fortune_cmd(client, message):
 		stdout, _stderr = await proc.communicate()
 	output = cleartermcolor(stdout.decode())
 	await edit_or_reply(message, "``` → " + output + "```")
+
+HELP.add_help(["iter_freq"], "find frequent words in messages",
+				"**[!]** --This will search with telegram API calls--\n" +
+				"find most used words in last messages. If no number is given, will search only " +
+				"last 100 messages. By default, 10 most frequent words are shown, but number of results " +
+				"can be changed with `-r`. By default, only words of `len > 3` will be considered. " +
+				"A minimum word len can be specified with `-min`. Will search in current group or any specified with `-g`. " +
+				"A single user can be specified with `-u` : only messages from that user will count if provided.",
+				args="[-r <n>] [-min <n>] [-g <group>] [-u <user>] [n]", public=True)
+@alemiBot.on_message(is_allowed & filterCommand(["iter_freq"], list(alemiBot.prefixes), options={
+	"results" : ["-r", "-res"],
+	"minlen" : ["-min"],
+	"group" : ["-g", "-group"],
+	"user" : ["-u", "-user"]
+}))
+@report_error(logger)
+@set_offline
+async def cmd_frequency(client, message):
+	results = int(message.command["results"]) if "results" in message.command else 10
+	number = int(message.command["cmd"][0]) if "cmd" in message.command else 100
+	min_len = int(message.command["minlen"]) if "minlen" in message.command else 3
+	group = None
+	if "group" in message.command:
+		val = message.command["group"]
+		group = await client.get_chat(int(val) if val.isnumeric() else val)
+	else:
+		group = message.chat
+	user = None
+	if "user" in message.command:
+		val = message.command["user"]
+		user = await client.get_users(int(val) if val.isnumeric() else val)
+	logger.info(f"Counting {results} most frequent words in last {number} messages")
+	response = await edit_or_reply(message, f"` → ` Counting word occurrences...")
+	words = []
+	count = 0
+	async for msg in client.iter_history(group.id, limit=number):
+		if not user or user.id == msg.from_user.id:
+			words += [ w for w in re.sub(r"[^0-9a-zA-Z\s\n]+", "", get_text(msg).lower()).split() if len(w) > min_len ]
+		count += 1
+		if count % 250 == 0:
+			await client.send_chat_action(message.chat.id, "playing")
+			await response.edit(f"` → [{count}/{number}] ` Counting word occurrences...")
+	count = Counter(words).most_common()
+	from_who = f"(from **{get_username(user)}**)" if user else ""
+	output = f"`→ {get_channel(group)}` {from_who}\n` → ` **{results}** most frequent words __(len > {min_len})__ in last **{number}** messages:\n"
+	for i in range(results):
+		output += f"`{i+1:02d}]{'-'*(results-i-1)}>` `{count[i][0]}` `({count[i][1]})`\n"
+	await response.edit(output, parse_mode="markdown")
+
